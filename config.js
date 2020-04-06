3126e9c60bb728a31f835ccfb923d83218936909
22368311+abidhkm@users.noreply.github.com <abidhone@gmail.com>
2020-04-06 15:58:48 +0400

    config changes

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
     manifestThemeColor: '#663399',
     manifestDisplay: 'standalone',
     manifestIcon: 'src/assets/img/website-icon.png',
-    pathPrefix: `/gatsby-starter-identity/`, // This path is subpath of your hosting https://domain/portfolio
+    pathPrefix: `/portfolio`, // This path is subpath of your hosting https://domain/portfolio
     authorName: 'Abidh Muhammed',
     heading: 'Full Stack Developer',
     contact: 'abidhone@gmail.com',
